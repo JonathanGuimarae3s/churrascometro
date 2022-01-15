a181f844a734689981f78a245edc64f838c06182
Jonathan <guimaraesjonathan92@gmail.com>
2022-01-14 21:59:05 -0300

    commit final

--- script.js
+++ script.js
@@ -1,63 +1,60 @@
 // alert('Pagina ainda em desenvolvimento. Em breve haverá uma atualização!')
-let inputAdulto= document.getElementById("adulto")
-let inputCrianca= document.getElementById("crianca")
-let inputDuracao= document.getElementById("duracao")
+let inputAdulto = document.getElementById("adulto");
+let inputCrianca = document.getElementById("crianca");
+let inputDuracao = document.getElementById("duracao");
 
-let btn = document.getElementById("button")
+let btn = document.getElementById("button");
 
-let resultado = document.getElementById("resultados")
+let resultado = document.getElementById("resultados");
 
 function calcular() {
-    let adulto = inputAdulto.value
-    let crianca = inputCrianca.value
-    let duracao = inputDuracao.value
-    
-    let carne = carnePP(duracao)
-    let cerva = cervaPP(duracao)
-    let refri = refriPP(duracao)
+	let adulto = inputAdulto.value;
+	let crianca = inputCrianca.value;
+	let duracao = inputDuracao.value;
 
+	let carne = carnePP(duracao);
+	let cerva = cervaPP(duracao);
+	let refri = refriPP(duracao);
 
-    let qdtCarne = carne * adulto + (400/2)* crianca;
-    let qdtCerva = cerva * adulto ;
-    let qdtRefri = refri* adulto + (400/2)* crianca;
-    
-    mostrar(qdtCarne,qdtCerva,qdtRefri)
+	let qdtCarne = carne * adulto + (400 / 2) * crianca;
+	let qdtCerva = cerva * adulto;
+	let qdtRefri = refri * adulto + (400 / 2) * crianca;
+
+	mostrar(qdtCarne, qdtCerva, qdtRefri);
 }
 
+function mostrar(qdtCarne, qdtCerva, qdtRefri) {
+	const calc = [
+		qdtCarne / 1000,
 
-function mostrar(qdtCarne,qdtCerva,qdtRefri) {
+		Math.ceil(qdtCerva / 355),
 
-    const calc=[qdtCarne/1000,
-        
-        Math.ceil(qdtCerva/355) ,
-        
-        Math.ceil(qdtRefri/2000)
-    ]
-    resultado.innerHTML ="<h3>Você precisará comprar</h3>"
-    resultado.innerHTML += `<p>${calc[0]} Kg de carne.</p>`
-    resultado.innerHTML+= `<p>${calc[1]} Latas de cerveja.</p>`
-    resultado.innerHTML +=`<p>${calc[2]} Bebidas de 2L.</p>`
+		Math.ceil(qdtRefri / 2000),
+	];
+	resultado.innerHTML = "<h3>Você precisará comprar</h3>";
+	resultado.innerHTML += `<p>${calc[0]} Kg de carne.</p>`;
+	resultado.innerHTML += `<p>${calc[1]} Latas de cerveja.</p>`;
+	resultado.innerHTML += `<p>${calc[2]} Bebidas de 2L.</p>`;
 }
 
-
-function  carnePP(duracao) {
-    if(duracao >=6){
-        return 650
-    }else{
-        return 400
-    }
-}    
-function  cervaPP(duracao) {
-    if(duracao >=6){
-        return 2000
-    }else{
-        return 1200
-    }
+function carnePP(duracao) {
+	if (duracao >= 6) {
+		return 650;
+	} else {
+		return 400;
+	}
 }
-function  refriPP(duracao) {
-    if(duracao >=6){
-        return 1500
-    }else{
-        return 1000
-    }
-}    
+function cervaPP(duracao) {
+	if (duracao >= 6) {
+		return 2000;
+	} else {
+		return 1200;
+	}
+}
+function refriPP(duracao) {
+	if (duracao >= 6) {
+		return 1500;
+	} else {
+		return 1000;
+	}
+}
